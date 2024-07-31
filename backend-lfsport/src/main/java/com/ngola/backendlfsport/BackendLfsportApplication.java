80496e1d4ebe54165cc524965840d808eb93bc0b
Antonio ngola <antoniongola10@gmail.com>
2024-07-31 16:33:04 +0100

    Initial configs

---
+++ backend-lfsport/src/main/java/com/ngola/backendlfsport/BackendLfsportApplication.java
@@ -0,0 +1,13 @@
+package com.ngola.backendlfsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLfsportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLfsportApplication.class, args);
+    }
+
+}
---
+++ backend-lfsport/src/test/java/com/ngola/backendlfsport/BackendLfsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngola.backendlfsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLfsportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
