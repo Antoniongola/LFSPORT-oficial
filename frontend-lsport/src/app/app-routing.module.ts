4f061a5b06e9a983a2994ad31b87e4743a40d182
Antonio ngola <antoniongola10@gmail.com>
2024-08-01 16:11:34 +0100

    front end: basic updates

--- frontend-lsport/src/app/app-routing.module.ts
+++ frontend-lsport/src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {JogadoresComponent} from "./components/jogadores/jogadores.component";
+import {TreinadoresComponent} from "./components/treinadores/treinadores.component";
+import {ServicosComponent} from "./components/servicos/servicos.component";
+import {IntermediacoesComponent} from "./components/intermediacoes/intermediacoes.component";
+import {SobreNosComponent} from "./components/sobre-nos/sobre-nos.component";
+import {JogadorDetalhadoComponent} from "./components/jogador-detalhado/jogador-detalhado.component";
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'jogadores', component:JogadoresComponent, children:[
+      {path:':jogadorId', component:JogadorDetalhadoComponent}
+    ]
+  },
+  {path:'intermediacoes', component:IntermediacoesComponent},
+  {path:'servicos', component:ServicosComponent},
+  {path:'sobre', component:SobreNosComponent},
+  {path:'treinadores', component:TreinadoresComponent}
+
 ];
 
 @NgModule({
--- frontend-lsport/src/app/app.module.ts
+++ frontend-lsport/src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { JogadoresComponent } from './components/jogadores/jogadores.component';
+import { TreinadoresComponent } from './components/treinadores/treinadores.component';
+import { JogadorDetalhadoComponent } from './components/jogador-detalhado/jogador-detalhado.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
+import { SobreNosComponent } from './components/sobre-nos/sobre-nos.component';
+import { IntermediacoesComponent } from './components/intermediacoes/intermediacoes.component';
+import { JogadorComponent } from './components/jogador/jogador.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    JogadoresComponent,
+    TreinadoresComponent,
+    JogadorDetalhadoComponent,
+    ServicosComponent,
+    SobreNosComponent,
+    IntermediacoesComponent,
+    JogadorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
