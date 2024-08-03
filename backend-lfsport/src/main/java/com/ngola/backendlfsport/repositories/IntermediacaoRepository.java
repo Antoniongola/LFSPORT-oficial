87d1290adde544af3a2f9cb62dc01a42d22cef65
antoniongola <antoniongola10@gmail.com>
2024-08-03 13:13:37 +0100

    update: backend - adding new feature for intermediacao repository.

--- backend-lfsport/src/main/java/com/ngola/backendlfsport/repositories/IntermediacaoRepository.java
+++ backend-lfsport/src/main/java/com/ngola/backendlfsport/repositories/IntermediacaoRepository.java
@@ -4,6 +4,9 @@ import com.ngola.backendlfsport.entities.Intermediacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IntermediacaoRepository extends JpaRepository<Intermediacao, Long> {
+    public List<Intermediacao> findAllByJogadorTransferidoEqualsIgnoreCase(String nome);
 }
